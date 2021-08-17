a1183cfa66bb30614d7e9479a7f53aa42e1235f1
Luiscaicedouwu <87402017+Luiscaicedouwu@users.noreply.github.com>
2021-08-17 13:28:41 -0500

    Update punto2.js

--- punto2.js
+++ punto2.js
@@ -1,6 +1,12 @@
-function es_divisor(n){
-    if (n % 2 == 0){
-    return console.log("El número " + n + " es divisor de 2") }
+// Punto numero 2 //
 
-    else console.log("El número " + n + " no es divisor de 2")
+function es_divisor(numero) {
+    if(numero % 2 === 0 ){
+        console.log("El numero " + numero + " es divisor" + 2)
+        
+    }
+    else console.log("El numero " + numero + " no es divisor" + 2)
+
 }
+
+es_divisor(5)
